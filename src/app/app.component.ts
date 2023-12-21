2db78aecfecea50d56575774fa3639739bdfc267
Bugra Okumus <bugra.okumus@d-teknoloji.com.tr>
2023-12-21 15:21:11 +0300

    feat: added bootstrap and jquery for clientside

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'E-TicaretClient';
+
+  ngOnInit() {
+    $(() => {
+      console.log('test');
+    });
+    $(document).ready(() => {
+      console.log('test 2');
+    });
+  }
 }
--- src/app/client/layout/components/header/header.component.ts
+++ src/app/client/layout/components/header/header.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-client-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
